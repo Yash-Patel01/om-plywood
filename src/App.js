e295c236d479a1690e3b3d101a28b2a994050cb2
Yash Kathrotiya <55884549+Yash-Patel01@users.noreply.github.com>
2022-03-06 11:38:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import backgroundImage from "./images/images.jpg";
+
+export default function App() {
+  return (
+    <div class="d-flex flex-row justify-content-center align-items-center main-background">
+      <div class="p-2 title-brand">
+        <span class="title">Om Plywood</span>
+      </div>
+    </div>
+  );
+}
